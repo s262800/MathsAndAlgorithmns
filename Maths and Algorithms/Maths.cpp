7e3c0d03ebdd35d2c70096e9a0501c7acd050f55
s262800 <s262800@uos.ac.uk>
2024-04-16 10:44:08 +0100

    radian degrees

--- Maths and Algorithms/Maths.cpp
+++ Maths and Algorithms/Maths.cpp
@@ -1,1 +1,50 @@
 #include "Maths.h"
+
+float MathUtils::toRad(float Deg)
+{
+	float radian = Deg * 3.14 / 180;
+	
+	return radian;
+}
+
+float MathUtils::toDeg(float Rad)
+{
+	float degrees = Rad * 180 / 3.14;
+	
+	return degrees;
+}
+
+float MathUtils::Pythagoras(float A, float B)
+{
+	return 0.0f;
+}
+
+float MathUtils::FindSinTheta(float A, float B)
+{
+	return 0.0f;
+}
+
+float MathUtils::FindCosTheta(float A, float B)
+{
+	return 0.0f;
+}
+
+float MathUtils::FindTanTheta(float A, float B)
+{
+	return 0.0f;
+}
+
+float MathUtils::FindHyp(float Angle, float B)
+{
+	return 0.0f;
+}
+
+float MathUtils::FindOpp(float Angle, float B)
+{
+	return 0.0f;
+}
+
+float MathUtils::FindAdj(float Angle, float B)
+{
+	return 0.0f;
+}
