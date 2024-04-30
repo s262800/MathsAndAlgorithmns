d69d1324632e3d0e3bbeec9ac49b07c66c490d4f
s262800 <s262800@uos.ac.uk>
2024-04-30 10:31:21 +0100

    function defs

--- Maths and Algorithms/Matrix.cpp
+++ Maths and Algorithms/Matrix.cpp
@@ -1,1 +1,36 @@
 #include "Matrix.h"
+
+float Matrix::Determinate()
+{
+  
+	
+	return 0.0f;
+}
+
+void Matrix::RotateAroundX(float Angle)
+{
+}
+
+void Matrix::RotateAroundY(float Angle)
+{
+}
+
+void Matrix::RotateAroundZ(float Angle)
+{
+}
+
+void Matrix::Translate(Vector Delta)
+{
+}
+
+void Matrix::RotateAroundX(Vector Point, float Angle)
+{
+}
+
+void Matrix::RotateAroundY(Vector Point, float Angle)
+{
+}
+
+void Matrix::RotateAroundZ(Vector Point, float Angle)
+{
+}
